1b05d49fe847e811a00c81899390641bdbc27b66
Hailey Murto and Matt Phillips <hailey.murto@gmail.com>
2016-02-24 20:59:09 -0800

    creates javascript for factorial site.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var factorial = function(number) {
+  for (var index = number; index = number.length; index - 1) {
+  (factorial[number]);
+}
+
+});
+
+var factorial = function(number) {
+for (i = 5; i <= 5; i--)
+{
+factorial *= i;
+}
